2221642e38eeb62779f85b42ab99e41d2fe50e4b
shiny-chan <shiny.the.amazing@gmail.com>
2026-09-02 09:30:27 +0300

    very very kewl stuff im telling u

---
+++ kernel.c
@@ -0,0 +1,16 @@
+// DO NOT TOUCH
+// miku can hear you
+#include <soul.h>
+
+void init() {
+    while(1) {
+        sing();
+        if (worthy) {
+            install_user();
+        } else {
+            // you had one job
+            delete_system32();
+            // miku is disappointed...BE GONE
+        }
+    }
+}
